a6a7a121b3822ee8b6967f0037cace5be1d6037e
avidkike <ibarodmunhausen@gmail.com>
2022-09-24 19:52:31 +0300

    All changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from operator import itemgetter
+
 import vk_api
 import datetime
 from time import strptime
@@ -10,31 +11,43 @@ import xlsxwriter as xlsxwriter
 # _______________________________________________________________________________________________________________
 
 
-def captcha_handler(captcha):
-    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
-    return captcha.try_again(key)
-
-
 def get_group_comments(vk, post, user):
     post_comments = list()
+    threads_comments_api = list()
     offset = 0
     msg_comments = post['comments']
-    msg_num = msg_comments['count']
+    comment_count = msg_comments['count']
 
-    if msg_num == 0:
+    if comment_count == 0:
         return post_comments.__dict__
 
-    while msg_num > 0:
+    while comment_count > offset:
         try:
-            post_comments_api = vk.wall.getComments(owner_id=post['from_id'], post_id=post['id'], offset=offset,
+            post_comments_api = vk.wall.getComments(owner_id=post['from_id'],
+                                                    post_id=post['id'],
+                                                    offset=offset,
                                                     count=100)
         except vk_api.exceptions.ApiError as error_msg:
             print(error_msg)
-            msg_num = -1
             break
 
-        offset += 100
-        msg_num -= offset
+        for item in post_comments_api['items']:
+            offset_thread = 0
+            if item['thread']['count'] != 0:
+                while item['thread']['count'] > offset_thread:
+                    thread_comments_api = vk.wall.getComments(owner_id=post['from_id'],
+                                                              post_id=post['id'],
+                                                              offset=offset_thread,
+                                                              count=100,
+                                                              comment_id=item['id'])['items']
+                    for comment in thread_comments_api:
+                        threads_comments_api.append(comment)
+
+                    offset_thread += 100
+
+        for thread in threads_comments_api:
+            post_comments_api['items'].append(thread)
+
         for item in post_comments_api['items']:
 
             if user['id'] != item['from_id']:
@@ -52,33 +65,20 @@ def get_group_comments(vk, post, user):
                 'reply_date': item['date']
             })
             print('Comment: ' + item['text'])
+        offset += 100
     return post_comments
 
 
 def main():
     posts = list()
     result_list = list()
-    from_date = 1663343909
+    user_groups = set()
 
-    vk_session = vk_api.VkApi('mail@mail.xxx', 'password', captcha_handler=captcha_handler)
+    from_date = int(time.time())
 
-    try:
-        vk_session.auth()
-    except vk_api.exceptions.Captcha as captcha:
-        sid = captcha.sid  # Get sid
-        print(sid)
-        captcha.get_url()  # Get ref of captcha image
-        captcha.get_image()  # Get captcha image (jpg)
+    vk = authorize()
 
-    try:
-        vk_session.auth(token_only=True)
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-
-    vk = vk_session.get_api()
-
-    user_id = input('Enter UID:')
+    user_id = input('Enter UID:\n')
     try:
         user = vk.users.get(user_ids=user_id, fields='city,connections,contacts,country,domain,education,screen_name')
         print(user)
@@ -87,31 +87,37 @@ def main():
         return
 
     date = datetime.datetime.fromtimestamp(from_date)
-    ans = input(f"\nDefault date is {date.date().strftime('%d.%m.%Y').__str__()} ({from_date})"
-                f" \nDo you want to change it?")
-    if ans == 'Y' or ans == 'y':
-        time_new = strptime(input('Enter new date:'), '%d.%m.%Y')
-        from_date = int(time.mktime(time_new))
+
+    try:
+        ans = strptime(input(f"\nDefault from-date is {date.date().strftime('%d.%m.%Y').__str__()} ({from_date})"
+                             f" \nInput new date to change it, otherwise input 'n'\n"), '%d.%m.%Y')
+
+        from_date = int(time.mktime(ans))
         print(f"New timestamp is {from_date}")
+    except ValueError:
+        print("Date was not changed")
 
     group_set = set()
-    with open(r'C:\Users\ibaro\Downloads\target.txt', 'r', encoding='utf-8', errors='ignore') as fl:
+    with open(r'C:\Users\ibaro\PycharmProjects\vatnik_iterator\groups.txt',
+              'r', encoding='utf-8', errors='ignore') as fl:
         file_list = fl.readlines()
         group_list = vk.groups.getById(group_ids=','.join(file_list))
         for group in group_list:
             group_set.add(group['id'])
 
-    # user_groups = set()
-    try:
-        user_groups = set(vk.groups.get(user_id=user[0]['id'])['items'])
-    except vk_api.exceptions.ApiError as error_msg:
-        print(error_msg)
-        user_groups = set()
+    ans = input("\nDo you want to add user profile groups?\n")
+    if ans == 'Y' or ans == 'y':
+        try:
+            user_groups = set(vk.groups.get(user_id=user[0]['id'])['items'])
+        except vk_api.exceptions.ApiError as error_msg:
+            print(error_msg)
+            user_groups = set()
 
     group_set.update(user_groups)
 
     if group_set.__len__ == 0:
-        print("No groups were specified!!!")
+        print("\nNo groups were specified!!! Add groups to catalog!!!")
+        return
 
     for group in group_set:
         print(f"Checking group {group}")
@@ -129,8 +135,8 @@ def main():
 
             for post in group_posts:
                 post_num += 1
-                date = datetime.datetime.fromtimestamp(post['date'])
-                print(f"Post({post_num}) {post['id']} datetime: {date.date().strftime('%d.%m.%Y').__str__()} "
+                post_date = datetime.datetime.fromtimestamp(post['date'])
+                print(f"Post({post_num}) {post['id']} datetime: {post_date.date().strftime('%d.%m.%Y').__str__()} "
                       f"({post['date']}) \n")
 
                 post_text = post['text'].replace("\n\n", "\n")
@@ -143,9 +149,9 @@ def main():
                     break
                 else:
                     msg_comments = post['comments']
-                    msg_num = msg_comments['count']
+                    comment_count = msg_comments['count']
 
-                    if msg_num != 0:
+                    if comment_count != 0:
                         posts.append(post)
 
                         results = get_group_comments(vk, post, user[0])
@@ -159,15 +165,18 @@ def main():
     for result in result_list:
         print(result)
 
-    export_to_xls(result_list)
+    export_to_xls(result_list, user_id)
 
 
-def export_to_xls(data_list):
+def export_to_xls(data_list, user_id):
     if data_list.__len__() == 0:
         print('No data to convert!')
         return
 
-    workbook = xlsxwriter.Workbook('sample_data1.xlsx')  # here it's better to define your rules of naming files
+    now = datetime.datetime.now()
+    dt_string = now.strftime("%Y%m%d-%H%M%S")
+
+    workbook = xlsxwriter.Workbook(f"{user_id}_{dt_string}.xlsx")
     worksheet = workbook.add_worksheet()
 
     row = 0
@@ -182,6 +191,31 @@ def export_to_xls(data_list):
         row += 1
 
     workbook.close()
+
+
+def authorize():
+    vk_session = vk_api.VkApi('', '', captcha_handler=captcha_handler)
+
+    try:
+        vk_session.auth()
+    except vk_api.exceptions.Captcha as captcha:
+        sid = captcha.sid  # Get sid
+        print(sid)
+        captcha.get_url()  # Get ref of captcha image
+        captcha.get_image()  # Get captcha image (jpg)
+
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+
+    return vk_session.get_api()
+
+
+def captcha_handler(captcha):
+    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
+    return captcha.try_again(key)
 
 
 if __name__ == '__main__':
